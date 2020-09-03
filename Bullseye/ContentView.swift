6e6c76c0b492bcac24df14cc38651a6d15ed0654
Rostyslav Broslavskyi <broslavsky.rostislaw@gmail.com>
2020-09-03 21:46:37 +0300

    Initial Commit

---
+++ Bullseye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Rostyslav Broslavskyi on 03.09.2020.
+//  Copyright © 2020 Rostyslav Broslavskyi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
